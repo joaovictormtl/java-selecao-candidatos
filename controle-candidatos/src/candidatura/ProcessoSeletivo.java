4e392cb06e46999b412272b74b773b9ffb5c4b3e
johnstack <joaovictormtl@gmail.com>
2024-07-31 18:42:10 -0300

    feat: modificar comportamento do método selecaoCandidatos

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,11 +1,42 @@
 package candidatura;
 
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) throws Exception {
-    
+        
+        String [] candidatos = {"JOÃO", "LUIZ", "REJANE", "ROSELITO", "YASMIM", "RAYANE", "REGINA", "RICARDO", "MÔNICA", "GABRIEL", "RICARDO", "HENNING", "NICOLAS", "ATHOS", "RAUL", "GUSTAVO", "DAVID", "LUCAS", "ELAINE", "DILERMANDO"};
 
+        String [] selecionados = selecaoCandidatos(candidatos);
+
+    }
+
+    static String [] selecaoCandidatos(String [] candidatos){
+        String [] selecionados = new String[5];
+        int indice = 0;
+        double salarioBase = 2000.0;
+
+        for(int i = 0; i < candidatos.length; i++){
+            String candidato = candidatos[i];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("Candidato(a) "+candidato+" solicitou R$"+salarioPretendido);
+
+            if(salarioBase >= salarioPretendido){
+                System.out.println("Candidato(a) "+candidato+" foi selecionado(a).\n");
+                selecionados[indice] = candidato;
+                indice++;
+                if(indice == 5) break;
+            } else{
+                System.out.println("Candidato(a) "+candidato+" não foi selecionado(a).\n");
+            }
+        }
+        if(indice < 5){
+            System.out.println("Menos de 5 candidatos foram selecionados...");
+        }
+        
+        return selecionados;
     }
 
     public static double valorPretendido(){
