92a087cd7668937709caf04690cac8f2cd9db01d
johnstack <joaovictormtl@gmail.com>
2024-09-15 15:53:01 -0300

    feat: add atributos e construtor da classe Candidato

--- controle-candidatos/src/candidatura/Candidato.java
+++ controle-candidatos/src/candidatura/Candidato.java
@@ -1,5 +1,14 @@
 package candidatura;
 
 public class Candidato {
-    
+    private static int contadorInscricao = 1000; 
+    private int numeroInscricao;
+    private String nome;
+    private double salarioPretendido;
+
+    public Candidato(String nome, double salarioPretendido){
+        this.numeroInscricao = contadorInscricao += 1000;
+        this.nome = nome;
+        this.salarioPretendido = 0;
+    }
 }
